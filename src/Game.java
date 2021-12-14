d678c32ead1168fcb11778053a41ccfdbb8514d0
Jennings <oliver.jennings@accenture.com>
2021-12-14 10:36:25 +0000

    Task 1 - Additional Details completed

--- src/Game.java
+++ src/Game.java
@@ -1,8 +1,36 @@
 public class Game {
 
-    public String gameName;
+    private int ID;
+    private String gameManufacturer;
+    private String gameName;
+    private double gamePrice;
+    private int quantity;
 
-    public Game(String gameName) {
+    public Game(int ID, String gameManufacturer, String gameName, double gamePrice, int quantity) {
+        this.ID = ID;
+        this.gameManufacturer = gameManufacturer;
         this.gameName = gameName;
+        this.gamePrice = gamePrice;
+        this.quantity = quantity;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public String getGameManufacturer() {
+        return gameManufacturer;
+    }
+
+    public String getGameName() {
+        return gameName;
+    }
+
+    public double getGamePrice() {
+        return gamePrice;
+    }
+
+    public int getQuantity() {
+        return quantity;
     }
 }
--- src/MainMenu.java
+++ src/MainMenu.java
@@ -1,5 +1,3 @@
-import com.sun.tools.javac.Main;
-
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -16,9 +14,10 @@ public class MainMenu {
     }
 
     private void populateStock() {
-        games.add(new Game("Halo"));
-        games.add(new Game("Minecraft"));
-        games.add(new Game("League of Legends1"));
+        games.add(new Game(121, "Bungie", "Halo Reach", 49.99, 10));
+        games.add(new Game(124, "Mojang", "Minecraft", 20.00, 3));
+        games.add(new Game(687, "Riot Games", "League of Legends", 0, 100));
+
     }
 
     private void startup() {
@@ -31,7 +30,7 @@ public class MainMenu {
         switch(choice) {
             case 1:
                 for(Game i : games) {
-                    System.out.println("Game: " + i.gameName);
+                    System.out.println("ID: " + i.getID() +  " - Manufacturer: " + i.getGameManufacturer() + " - Name: " + i.getGameName() + " - Price: " + i.getGamePrice() + " - Stock: " + i.getQuantity());
                 }
                 break;
             case 2:
