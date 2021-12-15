e1d2caaa1a4e28ce330244d3b84a6103c119275b
Jennings <oliver.jennings@accenture.com>
2021-12-15 11:21:18 +0000

    Task 3 - Customers Class added

---
+++ src/Customers.java
@@ -0,0 +1,19 @@
+public class Customers {
+
+    private String custName;
+    private int custAccNo;
+    private String custEmail;
+    private long custPhoneNo;
+    private String custAddress;
+
+    public Customers(String custName, int custAccNo, String custEmail, long custPhoneNo, String custAddress) {
+        this.custName = custName;
+        this.custAccNo = custAccNo;
+        this.custEmail = custEmail;
+        this.custPhoneNo = custPhoneNo;
+        this.custAddress = custAddress;
+    }
+
+
+
+}
